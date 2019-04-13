9f57882028035a3c33b005dd9e6dbea1a5a1805e
lauramann <lauramann@trentu.ca>
2019-04-13 17:04:25 -0400

    Login working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 import './App.css';
 import Login from './Login.js';
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  this.verifyUser = this.verifyUser.bind(this);
+  }
+  
+
+  verifyUser() {
+    
+  }
+
   render() {
+
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    console.log(user)
+
     return (
       <div className="App">
-        <Login />
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {
+            user 
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+          {
+            user ?
+            : user.displayName.endsWith(".trentu.ca") ?
+              <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+        </header>
       </div>
     );
+
+    // return (
+    //   <div className="App">
+    //     <Login />
+    //   </div>
+    // );
   }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBGDvQdCiWuHqvX8Q1LencFk9gO19l1-10",
+    authDomain: "trent-chemistry.firebaseapp.com",
+    databaseURL: "https://trent-chemistry.firebaseio.com",
+    projectId: "trent-chemistry",
+    storageBucket: "trent-chemistry.appspot.com",
+    messagingSenderId: "357251055536"
+  };
+
+  export default config;
