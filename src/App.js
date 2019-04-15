148031317c001dbb61cd346516065eb8ef75bc7c
lauramann <lauramann@trentu.ca>
2019-04-14 20:32:30 -0400

    Switching between faculty and student

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import withFirebaseAuth from 'react-with-firebase-auth'
-// import firebase from 'firebase';
-// import 'firebase/auth';
 import firebase from './firebaseConfig';
 import './App.css';
 import 'typeface-roboto';
 import Button from '@material-ui/core/Button';
 import FacultyDashboard from './FacultyDashboard';
+import StudentDashboard from './StudentDashboard';
 import "./App.css";
 
 const firebaseAppAuth = firebase.auth();
@@ -19,42 +18,56 @@ var usersRef = database.ref("users");
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isFaculty: null
+    }
     this.verifyUser = this.verifyUser.bind(this);
     this.writeUserData = this.writeUserData.bind(this)
     this.readUserData = this.readUserData.bind(this);
+    this.gotOne = this.gotOne.bind(this);
   }
 
-  writeUserData(email) {
-    let userName = email.split('@')[0]
-    let userRef = usersRef.child(userName)
-    userRef.update({isFaculty: false});
+  componentDidUpdate(prevProps) {
+    if (!prevProps.user && this.props.user) {
+      this.readUserData(this.props.user.email)
+    }
   }
 
   readUserData(email) {
     let userName = email.split('@')[0]
     let userRef = usersRef.child(userName)
-    
-    userRef.on("value", function(snapshot) {
-      console.log(snapshot.val());
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
+    userRef.on("value", this.gotOne);
+}
+
+  writeUserData(email) {
+    let userName = email.split('@')[0]
+    let userRef = usersRef.child(userName)
+
+    userRef.once('value', function (snapshot) {
+      if (!snapshot.hasChild('isFaculty')) {
+        userRef.update({ isFaculty: false });
+      }
     });
+  }
+
+  gotOne(data) {
+      this.setState({isFaculty: data.val().isFaculty})
   }
 
   verifyUser() {
     if (this.props.user && this.props.user.email.endsWith("@trentu.ca")) {
 
       this.writeUserData(this.props.user.email)
-      this.readUserData(this.props.user.email)
       return (
         <div>
           <Button variant="contained" color="primary" onClick={this.props.signOut}>Sign out</Button>
-          <FacultyDashboard name={this.props.user.displayName} email={this.props.user.email}/>
+          {this.state.isFaculty == true
+          ? <FacultyDashboard name={this.props.user.displayName} email={this.props.user.email}/> 
+          : <StudentDashboard name={this.props.user.displayName}/>}
         </div>
       );
     }
     else {
-      // console.log("not user")
       this.props.signOut()
       return (<Button variant="contained" color="primary" onClick={this.props.signInWithGoogle}>Sign in with Google</Button>)
 
@@ -62,19 +75,16 @@ class App extends Component {
   }
 
   render() {
-// console.log(firebaseApp.database())
     const {
       user,
       signOut,
       signInWithGoogle,
     } = this.props;
 
-    console.log(user)
 
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           {
             user
               ? <p></p> : <p>Please sign in.</p>
@@ -83,7 +93,6 @@ class App extends Component {
             this.verifyUser()
           }
         </header>
-        {/* {this.state.isAuth} ?  */}
       </div>
 
     );
--- src/StudentDashboard.js
+++ src/StudentDashboard.js
@@ -1,16 +1,12 @@
 import React, { PureComponent } from "react";
-// import { withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import CourseForm from './CourseForm';
-import QuizForm from './QuizForm';
 import firebase from './firebaseConfig';
 
 var database = firebase.database()
 
-class FacultyDashboard extends PureComponent {
+class StudentDashboard extends PureComponent {
   constructor(props) {
     super(props);
-    this.createCourse = this.createCourse.bind(this);
     this.gotOne = this.gotOne.bind(this);
   }
 
@@ -26,20 +22,12 @@ class FacultyDashboard extends PureComponent {
     // console.log(this.state.coursesArray)
   }
 
-  createCourse() {
-    console.log("Create Course :)")
-    return (<CourseForm />)
-  }
-
   render() {
     return (
       <div>
         <h1>Welcome, {this.props.name}</h1>
-        <Button variant="contained" color="primary" onClick={this.createCourse}>Add Course</Button>
-        <CourseForm email={this.props.email} />
-        <QuizForm />
       </div>
     );
   }
 }
-export default FacultyDashboard;
+export default StudentDashboard;
