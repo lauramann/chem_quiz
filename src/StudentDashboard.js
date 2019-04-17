f2ca56caa8a4d5db2ded6eeb75cc5ef71c42509d
lauramann <lauramann@trentu.ca>
2019-04-17 10:56:55 -0400

    Selecting class to see quizzes

--- src/StudentDashboard.js
+++ src/StudentDashboard.js
@@ -13,9 +13,12 @@ class StudentDashboard extends PureComponent {
     this.gotOne = this.gotOne.bind(this);
     this.state = {
       courses: {},
-      coursesArray: []
+      coursesArray: [],
+      courseCode: '',
+      chosenCourse: {}
     }
     this.showCourses = this.showCourses.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -25,45 +28,131 @@ class StudentDashboard extends PureComponent {
 
   gotOne(data) {
     let coursesArray = Object.keys(data.val())
-    this.setState({ 
+    this.setState({
       courses: data.val(),
       coursesArray: coursesArray
-     })
-    
+    })
+
   }
 
-  showCourses() {
-    let coursesArray = Object.keys(this.state.courses)
+  handleChange(event) {
+    let changeObj = {}
     let individualCourses = Object.values(this.state.courses)
-    let entries = Object.entries(this.state.courses)
-    let course1 = individualCourses[0]
-    let stateCourses = this.state.courses
-    let quizzes = []
-    console.log(entries)
-    let dataStruc = individualCourses[0]
-    // if(dataStruc != undefined)
-    //   console.log(Object.values(dataStruc.quizzes).name)
+    let chosen = event.target.value
+    let chosenCourse = {}
+    if (event.target.id) {
+        changeObj[event.target.id] = event.target.value
+    }
+    else changeObj[event.target.name] = event.target.value
 
-      if(individualCourses !=undefined){
-        individualCourses.map((course) =>(
-          <div>
-            {console.log(course.courseName)}
-          {Object.values(course.quizzes).map((quiz)=> (
-            <div>
-            { console.log(quiz.name) }
-             {Object.values(quiz).map((quiz) => (
-               <div>
-                {console.log(quiz)}
-                {console.log(quiz.question)}
-                {console.log(quiz.answer)}
-               </div>
-              
-             ))}
-            </div>
-          ))}
-          </div>
-        )) 
-      }
+    this.setState(changeObj);
+
+    if(individualCourses != undefined) {
+        individualCourses.map((course)=> {
+          if(course.courseCode == chosen) {
+            chosenCourse = course
+          }
+        })
+    }
+    console.log(chosenCourse)
+}
+
+  showCourses(chosenCourse) {
+    let individualCourses = Object.values(this.state.courses)
+    let courseObj = null
+
+    if(individualCourses != undefined) {
+
+        individualCourses.map((course)=> {
+          if(course.courseCode == chosenCourse) {
+            // <div>
+            courseObj = course
+            // return(<h2>Hello</h2>)
+              // {console.log(course)}
+            // <h2>{course.courseCode}: {course.courseName}</h2>
+            // </div>
+            
+            // <div>
+            //   {
+                // <h2>{course.courseCode}: {course.courseName}</h2>
+            //   }
+            // </div>
+          }
+        })
+
+    }
+    console.log(chosenCourse)
+    if(courseObj != null) {
+    return(
+      <div>
+        <h2>{courseObj.courseCode}: {courseObj.courseName}</h2>
+        {Object.values(courseObj.quizzes).map((quiz, i)=>(
+          <h3 key={i} >{quiz.name}</h3>
+        ))}
+      </div>
+    )}
+
+    // if(individualCourses != undefined) {
+    //   return(
+    //     individualCourses.map((course) => (
+    //       <div>
+    //         {course.courseCode != chosenCourse ? console.log("do not match") 
+    //         : <div>
+    //         <h2>{course.courseCode}: {course.courseName}</h2>
+    //         {Object.values(course.quizzes).map((quiz)=>(
+    //           <div>
+    //           {<h3>{quiz.name}</h3>}
+    //           {console.log(quiz.name)}
+    //           {Object.values(quiz).map((quiz) => (
+    //             <div>
+    //               {/* {<p>{quiz}</p>} */}
+    //               {console.log(quiz)}
+    //               {<h4>{quiz.question}</h4>}
+    //               {console.log(quiz.question)}
+    //               {<p>{quiz.answer}</p>}
+    //               {console.log(quiz.answer)}
+    //             </div>
+
+    //           ))}
+    //         </div>
+    //         ))}
+    //         </div>
+    //       }
+    //       </div>
+    //     )
+    //   ))
+    // }
+
+    // if (individualCourses != undefined) {
+    //   return (
+    //     individualCourses.map((course) => (
+    //       <div>
+    //         {console.log(course)}
+    //         {course.courseCode == chosenCourse ? console.log("match") : console.log("dont match")}
+    //         {console.log("chosen course: " +chosenCourse)}
+    //         {<h2>{course.courseName}</h2>}
+    //         {console.log(course.courseName)}
+    //         {Object.values(course.quizzes).map((quiz) => (
+    //           <div>
+    //             {<h3>{quiz.name}</h3>}
+    //             {console.log(quiz.name)}
+    //             {Object.values(quiz).map((quiz) => (
+    //               <div>
+    //                 {/* {<p>{quiz}</p>} */}
+    //                 {console.log(quiz)}
+    //                 {<h4>{quiz.question}</h4>}
+    //                 {console.log(quiz.question)}
+    //                 {<p>{quiz.answer}</p>}
+    //                 {console.log(quiz.answer)}
+    //               </div>
+
+    //             ))}
+    //           </div>
+    //         ))}
+    //       </div>
+    //     ))
+    //   )
+    // }
 
 
   }
@@ -72,7 +161,7 @@ class StudentDashboard extends PureComponent {
     // console.log(this.state.courses)
     return (
       <div>
-        {this.showCourses()}
+        
         <h1>Welcome, {this.props.name}</h1>
         <h2>Student Dashboard</h2>
         <InputLabel>Course</InputLabel>
@@ -88,7 +177,7 @@ class StudentDashboard extends PureComponent {
           {this.state.coursesArray.map((course) =>
             <MenuItem key={course} value={course}>{course}</MenuItem>)}
         </Select>
-        
+        {this.showCourses(this.state.courseCode)}
       </div>
     );
   }
