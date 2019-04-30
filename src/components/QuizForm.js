0d3e69e4b2823b925907604124d83c3644c935e0
lauramann <lauramann@trentu.ca>
2019-04-29 23:12:51 -0400

    Commented QuizForm

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -8,8 +8,23 @@ import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import '../styling/quizForm.css';
 
+// connect to database
 var database = firebase.database()
 
+// QuizForm class
+// Displays quiz
+// ----------------------------------------------------------------------------------
+// state explanations:
+//    question: string -> question created by user
+//    answer: string -> right answer created by user
+//    wrong1: string -> wrong answer created by user
+//    wrong2: string -> wrong answer created by user
+//    wrong3: string -> wrong answer created by user
+//    courseCode: string -> example: PHYS 2093
+//    coursesArray: bool -> array of courses from db
+//    name: string -> name of quiz created by user
+//    quiz: array -> quiz created by user
+// ----------------------------------------------------------------------------------
 class QuizForm extends PureComponent {
     constructor(props) {
         super(props);
@@ -25,17 +40,26 @@ class QuizForm extends PureComponent {
             quiz: []
         };
 
+        // declare methods
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.gotOne = this.gotOne.bind(this);
         this.handleAddQuestion = this.handleAddQuestion.bind(this);
     }
 
+    // set coursesRef to courses from db
+    // prevents infinite loop when calling setState
     componentDidMount() {
         let coursesRef = database.ref("courses")
         coursesRef.on("value", this.gotOne);
     }
 
+    // sets data from db to state
+    gotOne(data) {
+        this.setState({ coursesArray: Object.keys(data.val()) })
+    }
+
+    // set data from form to corresponding state variable
     handleChange(event) {
         let changeObj = {}
         if (event.target.id) {
@@ -46,10 +70,7 @@ class QuizForm extends PureComponent {
         this.setState(changeObj);
     }
 
-    gotOne(data) {
-        this.setState({ coursesArray: Object.keys(data.val()) })
-    }
-
+    // saves each question added by user into one object
     handleAddQuestion(event) {
         let quest = this.state.quiz
         quest.push({
@@ -59,15 +80,15 @@ class QuizForm extends PureComponent {
                 0: this.state.wrong1,
                 1: this.state.wrong2,
                 2: this.state.wrong3
-            }, 
+            },
             wrong1: this.state.wrong1,
             wrong2: this.state.wrong2,
             wrong3: this.state.wrong3
         })
 
-        this.setState({ quiz: quest })
-        document.getElementById("quiz-form").reset();
+        // save quiz to state and reset form
         this.setState({
+            quiz: quest,
             question: '',
             answer: '',
             wrong1: '',
@@ -76,14 +97,22 @@ class QuizForm extends PureComponent {
 
         })
 
+        document.getElementById("quiz-form").reset();
+
     }
 
+    // submitts quiz to datatbase
     handleSubmit(event) {
+        // ensures that the last question is added to quiz object
         this.handleAddQuestion()
+
+        // gets db references and creates "quizzes" in course in db
         let quizzesRef = database.ref("courses/" + this.state.courseCode)
         let quiz = quizzesRef.child('quizzes')
         let finalQuiz = this.state.quiz
         finalQuiz.name = this.state.name
+
+        // add full quiz to db
         quiz.push(finalQuiz);
         event.preventDefault();
     }
@@ -91,77 +120,78 @@ class QuizForm extends PureComponent {
     render() {
         return (
 
-                    <form id="quiz-form">
-                    <div className="select-course">
-                        <InputLabel>Course</InputLabel>
-                        <Select
-                            id="course"
-                            value={this.state.courseCode}
-                            onChange={this.handleChange}
-                            inputProps={{
-                                name: 'courseCode',
-                                id: 'course-code',
-                            }}
-                        >
-                            {this.state.coursesArray.map((course) =>
-                                <MenuItem key={course} value={course}>{course}</MenuItem>)}
-                        </Select>
-                        </div>
-                        <div className="name-quiz">
-                        <TextField
-                            id="name"
-                            label="Name of Quiz"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                            margin="normal"
-                        />
-                        </div>
-                        <div className="question-answer">
-                        <p>Create a question and provide the correct answer:</p>
-                        <TextField
-                            id="question"
-                            label="Question"
-                            value={this.state.question}
-                            onChange={this.handleChange}
-                            margin="normal"
-                        />
-                        
-                        <TextField
-                            id="answer"
-                            label="Correct Answer"
-                            value={this.state.answer}
-                            onChange={this.handleChange}
-                            margin="normal"
-                        />
-                        </div>
-                        <p>Provide some wrong answers:</p>
-                        <div className="options">
-                        <TextField
-                            id="wrong1"
-                            label="Option 1"
-                            value={this.state.wrong1}
-                            onChange={this.handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            id="wrong2"
-                            label="Option 2"
-                            value={this.state.wrong2}
-                            onChange={this.handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            id="wrong3"
-                            label="Option 3"
-                            value={this.state.wrong3}
-                            onChange={this.handleChange}
-                            margin="normal"
-                        />
-                        </div>
-                        <p>To add another question to this quiz, choose Add Question. When you're all done, click Submit.</p>
-                        <Button variant="contained" color="primary" onClick={this.handleAddQuestion}>Add Question</Button>
-                        <Button variant="contained" color="primary" onClick={this.handleSubmit}>Submit</Button>
-                    </form>
+            <form id="quiz-form">
+                <div className="select-course">
+                    <InputLabel>Course</InputLabel>
+                    <Select
+                        id="course"
+                        value={this.state.courseCode}
+                        onChange={this.handleChange}
+                        inputProps={{
+                            name: 'courseCode',
+                            id: 'course-code',
+                        }}
+                    >
+                        {/* loop through courses to display all courses */}
+                        {this.state.coursesArray.map((course) =>
+                            <MenuItem key={course} value={course}>{course}</MenuItem>)}
+                    </Select>
+                </div>
+                <div className="name-quiz">
+                    <TextField
+                        id="name"
+                        label="Name of Quiz"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+                </div>
+                <div className="question-answer">
+                    <p>Create a question and provide the correct answer:</p>
+                    <TextField
+                        id="question"
+                        label="Question"
+                        value={this.state.question}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+
+                    <TextField
+                        id="answer"
+                        label="Correct Answer"
+                        value={this.state.answer}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+                </div>
+                <p>Provide some wrong answers:</p>
+                <div className="options">
+                    <TextField
+                        id="wrong1"
+                        label="Option 1"
+                        value={this.state.wrong1}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="wrong2"
+                        label="Option 2"
+                        value={this.state.wrong2}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="wrong3"
+                        label="Option 3"
+                        value={this.state.wrong3}
+                        onChange={this.handleChange}
+                        margin="normal"
+                    />
+                </div>
+                <p>To add another question to this quiz, choose Add Question. When you're all done, click Submit.</p>
+                <Button variant="contained" color="primary" onClick={this.handleAddQuestion}>Add Question</Button>
+                <Button variant="contained" color="primary" onClick={this.handleSubmit}>Submit</Button>
+            </form>
         );
     }
 }
